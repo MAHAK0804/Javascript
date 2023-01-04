498d8237b07d8df7aaf633edb515d632f2b7cab8
MAHAK <111964960+MAHAK0804@users.noreply.github.com>
2023-01-04 21:45:08 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -708,3 +708,46 @@ if(x>y){
     }else
         console.log("Both have same BMI");
     
+
+
+
+
+
+
+/*********************
+  *  Loops and iteration
+  */ 
+   //for loop 
+ for(var i=0;i<10;i++)
+ {
+    console.log(i);
+ }
+
+
+ var john =["Mahak", "Garg",2004,"Engineer",false];
+ for(var i=0;i<john.length;i++){
+ console.log(john[i]);}
+//while loop
+ var i=0;
+ while(i<john.length)
+ {
+    console.log(john[i]);
+    i++; 
+ }
+
+ //continue and break statements
+ for(var i=0;i<john.length;i++)
+ {
+    if(typeof john[i] !=="string")continue;
+    console.log(john[i]);
+ }
+
+ for(var i=0;i<john.length;i++)
+ {
+    if(typeof john[i] !=="string")break;
+    console.log(john[i]);
+ }
+
+ //looping backwards
+ for(var i=john.length-1;i>=0;i--){
+    console.log(john[i]);}
